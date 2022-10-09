b3b1e784af56f3693689d18c84495ccdd7b6528f
thenesterov <thenesterov@proton.me>
2022-10-09 18:36:16 +0500

    Completed pz-7

---
+++ pz-7/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = {5, 3, -1, 8, 0, -6, 1};
+
+bool negative = false;
+int sum = 0;
+
+foreach (int number in array)
+{
+    if (!negative)
+    {
+        if (number < 0)
+        {
+            negative = true;
+        }
+    }
+    else
+    {
+        sum += Math.Abs(number);
+    }
+}
+
+Console.WriteLine(sum);
