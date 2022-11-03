524779b7c535decc86763383a5077077176f7448
thenesterov <thenesterov@proton.me>
2022-11-03 21:08:50 +0500

    Completed pz-10

---
+++ pz-10/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter the text: ");
+string[] input = Console.ReadLine().Split();
+
+foreach (string word in input)
+{
+    bool isNumber = true;
+    
+    foreach (var letter in word)
+    {
+        if (!char.IsNumber(letter))
+        {
+            isNumber = false;
+            break;
+        } 
+    }
+
+    if (isNumber)
+    {
+        Console.WriteLine(word);
+    }
+}
