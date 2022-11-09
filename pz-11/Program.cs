7762517c3b7896ca69737bcb5a41e18a3e7be4a4
thenesterov <thenesterov@proton.me>
2022-11-09 20:13:59 +0500

    Completed pz-11

---
+++ pz-11/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] sentences = Console.ReadLine().Split(". ");
+
+foreach (string word in sentences[0].Split())
+{
+    if (sentences[1].Split().Contains(word))
+    {
+        Console.WriteLine(word);
+    }
+}
