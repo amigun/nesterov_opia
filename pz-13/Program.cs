3b4e6cf821e68b48d2baf9439a9896f8594d8cdd
thenesterov <thenesterov@proton.me>
2022-11-16 18:06:18 +0500

    Complete pz-13

---
+++ pz-13/Program.cs
@@ -0,0 +1,22 @@
+﻿class pz_13
+{
+    public static int CountOfPowersOfTwo(int[] numbers)
+    {
+        int counter = 0;
+        
+        foreach (int number in numbers)
+        {
+            if (number != 1 && Math.Log(number, 2) * 10 % 10 == 0)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CountOfPowersOfTwo(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}));
+    }
+}
