4b902e1b536d7c7f5da9c48bc4febda0e15f09bd
thenesterov <thenesterov@proton.me>
2022-12-19 21:42:10 +0500

    Complete pz-15

---
+++ pz-15/Program.cs
@@ -0,0 +1,17 @@
+﻿// 17
+
+using (FileStream fstream = File.OpenRead("../../../file.txt"))
+{
+    StreamReader sreader = new StreamReader(fstream);
+
+    int count_lines = 0, count_words = 0;
+    
+    foreach(string line in sreader.ReadToEnd().Split('\n'))
+    {
+        Console.WriteLine(line);
+        count_lines++;
+        count_words += line.Split().Length;
+    }
+    
+    Console.WriteLine($"Count of lines: {count_lines}\nCount of words: {count_words}");
+}
