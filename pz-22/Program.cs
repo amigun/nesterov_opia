9f570a94cb95492a5f8224e2fb82bddd01469f5e
thenesterov <thenesterov@proton.me>
2023-05-09 16:26:52 +0500

    Completed pz-22

---
+++ pz-22/Program.cs
@@ -0,0 +1,24 @@
+﻿using pz_22;
+
+static void Print()
+{
+    Console.WriteLine($"\nКоличество\n\tвекторов: {Vector.GetCountOfVectors()}\n\tнулевых векторов: {Vector.GetCountOfZeroVectors()}\n");
+}
+
+Vector a = new Vector();
+Vector b = new Vector(2);
+Vector c = new Vector(30, 40);
+
+Print();
+
+Console.WriteLine("Делаем один из векторов нулевым...");
+b.X = 0;
+
+Print();
+
+Console.WriteLine("Опять делаем его ненулевым...");
+b.Y = 10;
+
+Print();
+
+
