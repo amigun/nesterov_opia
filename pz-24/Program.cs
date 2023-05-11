a7fcdb824b3b5542b9da3449751b62cde188ead6
thenesterov <thenesterov@proton.me>
2023-05-11 12:48:07 +0500

    Completed pz-24

---
+++ pz-24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using pz_23;
+
+Vector vec_1 = new Vector(2, 3);
+Vector vec_2 = (Vector)vec_1.Clone();
+
+vec_2.Y = 5;
+
+HorizontalVector hvec_1 = new HorizontalVector(5);
+HorizontalVector hvec_2 = (HorizontalVector)hvec_1.Clone();
+
+hvec_2.X = 10;
+
+Console.WriteLine(" === vec_1 === ");
+vec_1.PrintInfo();
+Console.WriteLine();
+
+Console.WriteLine(" === vec_2 === ");
+vec_2.PrintInfo();
+Console.WriteLine();
+
+Console.WriteLine(" === hvec_1 === ");
+hvec_1.PrintInfo();
+Console.WriteLine();
+
+Console.WriteLine(" === hvec_2 === ");
+hvec_2.PrintInfo();
+Console.WriteLine();
